cb115213d5d54151cbd3f77fc04608895b748dc9
yebin <yebind3308@gmail.com>
2023-08-10 03:11:13 +0900

    💄 헤더 레이아웃 작업 중

--- clientes/src/App.js
+++ clientes/src/App.js
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import './index.css';
 import Main from './pages/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <div className="w-full">
       <Header />
       <Routes>
         <Route path="/" element={<Main />}></Route>
