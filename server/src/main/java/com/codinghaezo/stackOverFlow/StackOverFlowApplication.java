11eac7aad338eed0206f0b187f09f9cb7bf75243
yang-jin-ho <yxino96@gmail.com>
2023-08-16 11:54:11 +0900

    [Refactor] Basic

--- server/src/main/java/com/codinghaezo/stackOverFlow/StackOverFlowApplication.java
+++ server/src/main/java/com/codinghaezo/stackOverFlow/StackOverFlowApplication.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableJpaAuditing
 public class StackOverFlowApplication {
 
 	public static void main(String[] args) {
