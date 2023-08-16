e6ca995e90b9b7da27d3bfdfd58185d2b9958038
yang-jin-ho <yxino96@gmail.com>
2023-08-16 11:00:40 +0900

    [Refactor] 답변기능 CRUD

--- server/build/generated/sources/annotationProcessor/java/main/com/codinghaezo/stackOverFlow/answer/AnswerMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com/codinghaezo/stackOverFlow/answer/AnswerMapperImpl.java
@@ -3,12 +3,14 @@ package com.codinghaezo.stackOverFlow.answer;
 import com.codinghaezo.stackOverFlow.answer.AnswerDto.AnswerCreateDto;
 import com.codinghaezo.stackOverFlow.answer.AnswerDto.AnswerResponseDto;
 import com.codinghaezo.stackOverFlow.answer.AnswerDto.AnswerUpdateDto;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.processing.Generated;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-14T11:26:20+0900",
+    date = "2023-08-16T05:58:15+0900",
     comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.2.1.jar, environment: Java 11.0.18 (Azul Systems, Inc.)"
 )
 @Component
@@ -52,5 +54,19 @@ public class AnswerMapperImpl implements AnswerMapper {
         answerResponseDto.setContent( answer.getContent() );
 
         return answerResponseDto;
+    }
+
+    @Override
+    public List<AnswerResponseDto> answersToAnswerResponseDtos(List<Answer> answers) {
+        if ( answers == null ) {
+            return null;
+        }
+
+        List<AnswerResponseDto> list = new ArrayList<AnswerResponseDto>( answers.size() );
+        for ( Answer answer : answers ) {
+            list.add( answerToAnswerResponseDto( answer ) );
+        }
+
+        return list;
     }
 }
