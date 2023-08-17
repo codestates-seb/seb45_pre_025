b1f2c3029e87b2f4a52d493b06d657b49431635a
yebin <yebind3308@gmail.com>
2023-08-17 09:39:04 +0900

    [Feat] 푸터 이슈 수정, 유저정보 페이지 구현

--- clientes/src/pages/Main.js
+++ clientes/src/pages/Main.js
@@ -4,8 +4,8 @@ const Main = () => {
   return (
     <main className="relative min-h-screen">
       <section>
-        <div className="max-w-[1950px] p-12 absolute top-14 w-full border">
-          <div className="bg-gray-700 rounded-md flex justify-center flex-col p-8 h-full">
+        <div className="relative top-14 w-full flex justify-between my-0 mx-auto p-12">
+          <div className="bg-gray-700 rounded-md flex justify-center flex-col p-8 h-full  w-full">
             {/* 말풍선 박스 */}
             <div className="flex justify-center">
               {/* 왼쪽 배너 */}
