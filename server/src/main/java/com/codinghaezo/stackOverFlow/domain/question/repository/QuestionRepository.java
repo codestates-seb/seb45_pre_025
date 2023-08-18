dfbef26b5372612b7c72fbcd683c94fde0e10f4b
yebin <yebind3308@gmail.com>
2023-08-18 18:11:21 +0900

    [feat] 서버삭제

--- server/src/main/java/com/codinghaezo/stackOverFlow/domain/question/repository/QuestionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.codinghaezo.stackOverFlow.domain.question.repository;
-
-import com.codinghaezo.stackOverFlow.domain.question.entity.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {}
